5c4c7ec9b48eca92d65a78566edceb52f4065630
Simon.Arnar <simon.arnar@gmail.com>
2023-12-07 12:25:02 +0100

    Börjat med att implementera en lista och konstruktor i csCupOfDice.cs

--- M3_Yahtzee/csCupOfDice.cs
+++ M3_Yahtzee/csCupOfDice.cs
@@ -9,11 +9,23 @@ namespace M3_Yahtzee
     /// Cup with 1 to 10 dice
     /// </summary>
 
-    /*
+    
     class csCupOfDice : ICupOfDice
     {
         csSeedGenerator _rnd = new csSeedGenerator();
-        protected List<enDiceFace> _dices;
+
+        protected List<enDiceFace> _dices = new List<enDiceFace>();
+        public int Count => _dices.Count;
+
+
+        public csCupOfDice(int NrOfDices)
+        {
+            if (NrOfDices < 1 || NrOfDices > 10)
+            {
+                throw new ArgumentException("Number of dice must be between 1 and 10.");
+            }
+
+        }
     }
-    */
+    
 }
